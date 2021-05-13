fa1d6f423e82fe6ea9e9a7fe1fa0de158e72bfba
yarden perets <yordenperets@gmail.com>
2021-05-13 15:23:08 +0000

    Feature/room-manager

---
+++ Trivia/Trivia/RoomManager.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include "LoggedUser.h"
+#include "ExceptionHandler.h"
+
+typedef struct RoomData 
+{
+	unsigned int id;
+	std::string name;
+	unsigned int maxPlayers;
+	unsigned int numOfQuestionsInGame;
+	unsigned int timePerQuestion;
+	unsigned int isActive;
+}RoomData;
+
+class Room 
+{
+public:
+	Room(LoggedUser admin, RoomData metadata);
+	~Room();
+	void addUser(LoggedUser user);
+	void removeUser(LoggedUser user);
+	std::vector<std::string> getAllUsers();
+	RoomData getMetadata() const;
+private:
+	RoomData m_metadata;
+	std::vector<LoggedUser> m_users;
+};
+
+class RoomManager
+{
+public:
+	RoomManager();
+	~RoomManager();
+	void createRoom(LoggedUser admin, RoomData metadata);
+	void deleteRoom(int roomID);
+	bool getRoomState(int roomID);
+	std::vector<RoomData> getRooms();
+private:
+	std::map<int, Room> m_rooms;
+};
