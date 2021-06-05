2d7d8f06c69d0a56fa1d37d4d25dc13f16139aac
yarden perets <yordenperets@gmail.com>
2021-06-05 06:51:48 +0000

    Feature/Game_manager

---
+++ Trivia/Trivia/Game.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include "IDatabase.h"
+#include "LoggedUser.h"
+#include "Question.h"
+struct GameData
+{
+	Question currentQuestion;
+	unsigned int correctAnswerCount;
+	unsigned int wrongAnswerCount;
+	unsigned int averageAnswerTime;
+};
+bool operator<(const LoggedUser& n1, const LoggedUser& n2)
+{
+	
+	LoggedUser p1 = n1;
+	LoggedUser p2 = n2;
+	return p1.getUsername().length() < p2.getUsername().length();
+}
+class Game
+{
+private:
+	std::vector<Question> m_question;
+	std::map<LoggedUser, GameData> m_players;
+	int gameId;
+public:
+	Game(std::list<Question> questions, std::vector<std::string> users, int gameId);
+	Question getQuestionForUser(LoggedUser user);
+	void submitAnswer(LoggedUser user, std::string answer);
+	void removePlayer(LoggedUser user);
+	std::map<LoggedUser, GameData>& getGameResults();
+	int getGameId()
+	{
+		return this->gameId;
+	}
+
+
+};
---
+++ Trivia/Trivia/GameManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "IDatabase.h";
+#include "Game.h"
+#include <vector>
+#include <list>
+#include <map>
+#include "RoomManager.h"
+class GameManager
+{
+private:
+	IDatabase* m_database;
+	std::vector<Game> m_games;
+public:
+	GameManager(IDatabase* database);
+	Game CreateGame(Room room);
+	void deleteGame(Game game);
+	void submitAnswer(Game game,LoggedUser user, std::string answer, int answerTime);
+	std::map<LoggedUser, GameData>& getGameResults(Game game);
+	
+
+};
