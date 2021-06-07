e89954f154941f04fc578d90a3a77bd96e9a1f3b
Guy Glick <guyglick27@gmail.com>
2021-06-07 16:51:01 +0300

    question update

--- Trivia/Trivia/Question.h
+++ Trivia/Trivia/Question.h
@@ -3,13 +3,13 @@
 class Question
 {
 private:
-	char* _q;
-	char* correctAns;
-	char* ans2;
-	char* ans3;
-	char* ans4;
+	std::string _q;
+	std::string correctAns;
+	std::string ans2;
+	std::string ans3;
+	std::string ans4;
 public:
-	Question(char* q, char* correct, char* ans2, char*, char* ans4) :
+	Question(std ::string q, std::string correct, std::string ans2, std::string, std::string ans4) :
 		_q(q), correctAns(correct), ans2(ans2), ans3(ans3), ans4(ans4)
 	{
 	}
@@ -19,11 +19,11 @@ public:
 	}
 	std::string getPossibleAnswers()
 	{
-		return std::string(ans2) + "&" +std::string(ans3) + "&" + std::string(ans4);
+		return ans2 + "&" +ans3 + "&" + ans4;
 	}
 	std::string getCorrectAnswer()
 	{
-		return std::string(correctAns);
+		return correctAns;
 	}
 	bool operator==(Question const& other) const
 	{
